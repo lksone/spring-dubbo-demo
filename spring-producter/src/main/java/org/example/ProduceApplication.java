bed87a25751fb2a59d7b2b2f1bd552a73605c1b6
a-likesheng <1056224715@qq.com>
2022-04-01 23:06:11 +0800

    this is demo

--- spring-producter/src/main/java/org/example/ProduceApplication.java
+++ spring-producter/src/main/java/org/example/ProduceApplication.java
@@ -1,9 +1,13 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.SpringVersion;
 
 /**
  * Hello world!
@@ -14,7 +18,10 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource(value = {"classpath:/spring/spring-producer.xml"})
 public class ProduceApplication {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
+
+      /*  new SpringApplicationBuilder(ProduceApplication.class).main(SpringVersion.class)
+                .bannerMode(Banner.Mode.CONSOLE).run(args);*/
         SpringApplication.run(ProduceApplication.class);
         log.info("启动成功");
     }
